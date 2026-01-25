4810d4d16f97b85df5d5babbc03074cbbe6e4940
Daniil Iaitskov <dyaitskov@gmail.com>
2026-01-25 21:04:29 +0300

    cert is ok, but no guard verification

---
+++ infix-subgoals.lisp
@@ -0,0 +1,133 @@
+; I haven't found an applicable code in ACL2 for doing infix translation and
+; I decided to make a simple function from scratch. After all I don't need to
+; translate everything - just implication and other basic logical operators
+; would make my day
+
+(in-package "ACL2")
+
+(include-book "std/strings/pretty" :dir :system)
+(include-book "std/basic/two-nats-measure" :dir :system)
+(include-book "tools/prettygoals/top" :dir :system :TTAGS :all)
+
+(defun sexp-depth (s)
+  (declare (xargs :guard t))
+  (if (consp s)
+      (+ 1
+         (max (sexp-depth (car s))
+              (sexp-depth (cdr s))))
+      0))
+
+(defun wrap-par (v outer-priority inner-priority)
+  (declare (xargs :guard (and (stringp v) (natp outer-priority) (natp inner-priority))))
+  (if (> outer-priority inner-priority)
+      (concatenate 'string "(" v ")")
+      v))
+
+(defun mypretty (x) (str::pretty x))
+
+(defun me (x y)
+  ;(declare (xargs
+     ;       :guard (and (listp x) (natp y))
+            ; :verify-guards nil
+      ;      ))
+  (nat-list-measure (list (sexp-depth x) (len x) y)))
+
+; (verify-guards me)
+
+(mutual-recursion
+ (defun infixargs (op args op-priority)
+   (declare (xargs :measure (me args 0)))
+   (if (consp args)
+       (if (consp (cdr args))
+           (concatenate 'string
+                        (infix2 (car args) op-priority)
+                        " " op " "
+                        (infixargs op (cdr args) op-priority))
+           (infix2 (car args) op-priority))
+       ""))
+
+ (defun infixargs-wrap (sexp outer-priority op inner-priority)
+   (declare (xargs
+             :measure (me sexp 7)))
+   (wrap-par (infixargs op sexp inner-priority)
+             outer-priority
+             inner-priority))
+
+ (defun infixfuncall (fargs)
+   (declare (xargs :measure (me fargs 8)))
+   (if (consp fargs)
+       (concatenate 'string
+                    " "
+                    (infix2 (car fargs) 100)
+                    (infixfuncall (cdr fargs)))
+       ""))
+
+ (defun infix2 (sexp outer-priority)
+   (declare (xargs :measure (me sexp 9)))
+   (case-match sexp
+       (('iff . a) (infixargs-wrap a outer-priority "↔" 10))
+     (('implies . a) (infixargs-wrap a outer-priority "→" 20))
+     (('or . a) (infixargs-wrap a outer-priority "∨" 30))
+     (('and . a) (infixargs-wrap a outer-priority "∧" 40))
+     (('xor . a) (infixargs-wrap a outer-priority "⊕" 45))
+     (('equal . a) (infixargs-wrap a outer-priority "=" 50))
+     (('< . a) (infixargs-wrap a outer-priority "<" 51))
+     (('> . a) (infixargs-wrap a outer-priority ">" 51))
+     (('<= . a) (infixargs-wrap a outer-priority "≤" 51))
+     (('>= . a) (infixargs-wrap a outer-priority "≥" 51))
+     (('cons a b)
+      (wrap-par (concatenate 'string
+                             (infix2 a 55)
+                             ":"
+                             (infix2 b 55))
+                outer-priority
+                55))
+     (('+ . a) (infixargs-wrap a outer-priority "+" 61))
+     (('- . a) (infixargs-wrap a outer-priority "-" 61))
+     (('* . a) (infixargs-wrap a outer-priority "*" 71))
+     (('/ . a) (infixargs-wrap a outer-priority "÷" 71))
+     (('nth i l)
+      (wrap-par
+       (concatenate 'string
+                    (infix2 l 85)
+                    "[" (infix2 i 0) "]" )
+       outer-priority
+       85))
+     (('not a)
+      (wrap-par
+       (concatenate 'string "¬" (infix2 a 90))
+       outer-priority
+       90))
+     (t "t")
+     (nil "nil")
+     (& (if (and (consp sexp) (symbolp (car sexp)))
+            (wrap-par
+             (concatenate 'string
+                         (str::pretty (car sexp))
+                         (infixfuncall (cdr sexp)))
+             outer-priority
+             99)
+            (mypretty sexp))))))
+
+
+(defun infix (sexp)
+  ;; (declare (xargs :guard t :verify-guards nil))
+  (infix2 sexp 0))
+
+; (verify-guards infix)
+(defconst *sample* '(IMPLIES
+                     (AND (CONSP L)
+                      (< 0 (+ 1 (LEN (CDR L))))
+                      (<= 0 X)
+                      (< X (+ 1 (LEN (CDR L))))
+                      (NOT (ZP X))
+                      (EQUAL (NTH (+ -1 X) (CDR L)) " "))
+                     (EQUAL (CAR L) " ")))
+
+
+;; defattach is commented due missing guard verification
+;; (defattach acl2::post-untranslate-hook infix)
+
+;; ACL2 Error in ( DEFATTACH POST-UNTRANSLATE-HOOK INFIX):  Attachments
+;; must be guard-verified function symbols (unless :SKIP-CHECKS T is specified
+;; with an active trust tag), but INFIX has not had its guard verified.
